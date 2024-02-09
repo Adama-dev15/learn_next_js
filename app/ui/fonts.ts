8dacffcccc7ed406f14ed73afdd31e0c276f7459
TRAORE ADAMA <traoreadamakoleba@gmail.com>
2024-02-09 06:37:39 +0100

    learn_next_js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Khand, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const khand = Khand({ subsets: ['latin'], weight: ['500'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['700'] });
